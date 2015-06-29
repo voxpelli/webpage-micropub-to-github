cf262be62f8c9c669a89cd80cb5c07533dce2371
Pelle Wessman <pelle@kodfabrik.se>
2015-06-29 17:26:21 +0200

    Format file names

--- test/formatter.spec.js
+++ test/formatter.spec.js
@@ -14,21 +14,23 @@ chai.should();
 describe('Formatter', function () {
   var MicropubFormatter = require('../lib/formatter.js');
   var formatter;
+  var baseMicropubData;
 
   beforeEach(function () {
     formatter = new MicropubFormatter();
+    baseMicropubData = {
+      'type': ['h-entry'],
+      'properties': {
+        'content': ['hello world'],
+        'name': ['awesomeness is awesome'],
+      },
+    };
   });
 
   describe('format', function () {
 
     it('should return a fully formatted page on sunny day content', function () {
-      return formatter.format({
-        'type': ['h-entry'],
-        'properties': {
-          'content': ['hello world'],
-          'name': ['awesomeness is awesome'],
-        },
-      }).should.eventually.equal(
+      return formatter.format(baseMicropubData).should.eventually.equal(
         '---\n' +
         'layout: post\n' +
         'date: \'2015-04-05T16:20:00+02:00\'\n' +
@@ -39,12 +41,9 @@ describe('Formatter', function () {
     });
 
     it('should handle non-existing title', function () {
-      return formatter.format({
-        'type': ['h-entry'],
-        'properties': {
-          'content': ['hello world'],
-        },
-      }).should.eventually.equal(
+      delete baseMicropubData.properties.name;
+
+      return formatter.format(baseMicropubData).should.eventually.equal(
         '---\n' +
         'layout: post\n' +
         'date: \'2015-04-05T16:20:00+02:00\'\n' +
@@ -54,12 +53,9 @@ describe('Formatter', function () {
     });
 
     it('should handle non-existing content', function () {
-      return formatter.format({
-        'type': ['h-entry'],
-        'properties': {
-          'name': ['awesomeness is awesome'],
-        },
-      }).should.eventually.equal(
+      delete baseMicropubData.properties.content;
+
+      return formatter.format(baseMicropubData).should.eventually.equal(
         '---\n' +
         'layout: post\n' +
         'date: \'2015-04-05T16:20:00+02:00\'\n' +
@@ -69,4 +65,24 @@ describe('Formatter', function () {
     });
 
   });
+
+  describe('formatFilename', function () {
+
+    it('should base file name on title', function () {
+      return formatter.formatFilename(baseMicropubData).should.eventually.equal('2015-04-05-awesomeness-is-awesome.html');
+    });
+
+    it('should fall back on content', function () {
+      delete baseMicropubData.properties.name;
+      return formatter.formatFilename(baseMicropubData).should.eventually.equal('2015-04-05-hello-world.html');
+    });
+
+    it('should ulimately fallback to just date', function () {
+      delete baseMicropubData.properties.name;
+      delete baseMicropubData.properties.content;
+      return formatter.formatFilename(baseMicropubData).should.eventually.equal('2015-04-05.html');
+    });
+
+  });
+
 });
