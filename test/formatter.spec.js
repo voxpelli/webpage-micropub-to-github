cc9ea5a67669448c5c18b6c9bd6272f220b77c60
Pelle Wessman <pelle@kodfabrik.se>
2015-06-29 15:44:13 +0200

    Some improvements + more tests

--- test/formatter.spec.js
+++ test/formatter.spec.js
@@ -21,20 +21,50 @@ describe('Formatter', function () {
 
   describe('format', function () {
 
-    it('should return a fully formatted page', function () {
+    it('should return a fully formatted page on sunny day content', function () {
       return formatter.format({
         'type': ['h-entry'],
         'properties': {
           'content': ['hello world'],
           'name': ['awesomeness is awesome'],
         },
-      }).should.equal(
+      }).should.eventually.equal(
         '---\n' +
         'layout: post\n' +
         'date: \'2015-04-05T16:20:00+02:00\'\n' +
         'title: awesomeness is awesome\n' +
         '---\n' +
         'hello world\n'
+      );
+    });
+
+    it('should handle non-existing title', function () {
+      return formatter.format({
+        'type': ['h-entry'],
+        'properties': {
+          'content': ['hello world'],
+        },
+      }).should.eventually.equal(
+        '---\n' +
+        'layout: post\n' +
+        'date: \'2015-04-05T16:20:00+02:00\'\n' +
+        '---\n' +
+        'hello world\n'
+      );
+    });
+
+    it('should handle non-existing content', function () {
+      return formatter.format({
+        'type': ['h-entry'],
+        'properties': {
+          'name': ['awesomeness is awesome'],
+        },
+      }).should.eventually.equal(
+        '---\n' +
+        'layout: post\n' +
+        'date: \'2015-04-05T16:20:00+02:00\'\n' +
+        'title: awesomeness is awesome\n' +
+        '---\n'
       );
     });
 
