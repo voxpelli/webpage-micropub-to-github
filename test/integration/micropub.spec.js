bdf2be9d53de077f696ee7fc3a04f741e83ec2f6
Pelle Wessman <pelle@kodfabrik.se>
2015-06-29 23:43:19 +0200

    Better micropub endpoint tests

--- test/integration/micropub.spec.js
+++ test/integration/micropub.spec.js
@@ -15,7 +15,33 @@ describe('Micropub API', function () {
   var express = require('express');
   var micropub = require('../../lib/micropub.js');
 
-  var app, agent;
+  var app, agent, token;
+
+  var mockTokenEndpoint = function (code, response) {
+    return nock('https://tokens.indieauth.com/')
+      .get('/token')
+      .reply(
+        code || 200,
+        response || 'me=http%3A%2F%2Fkodfabrik.se%2F&scope=post',
+        { 'Content-Type': 'application/x-www-form-urlencoded' }
+      );
+  };
+
+  var doRequest = function (mock, done, code, content) {
+    agent
+      .post('/micropub/thepost.se')
+      .set('Authorization', 'Bearer ' + token)
+      .type('form')
+      .send(content || {
+        h: 'entry',
+        content: 'hello world',
+      })
+      .expect(code || 201, function (err) {
+        if (err) { return done(err); }
+        mock.done();
+        done();
+      });
+  };
 
   beforeEach(function () {
     nock.disableNetConnect();
@@ -28,6 +54,8 @@ describe('Micropub API', function () {
     app.use('/micropub', micropub());
 
     agent = request.agent(app);
+
+    token = 'abc123';
   });
 
   afterEach(function () {
@@ -58,32 +86,50 @@ describe('Micropub API', function () {
   describe('auth', function () {
 
     it('should call handler and return 201 on successful request', function (done) {
-      var token = 'abc123';
-
       var mock = nock('https://tokens.indieauth.com/')
         .matchHeader('Authorization', function (val) { return val && val[0] === 'Bearer ' + token; })
-        .get('/token')
         .matchHeader('Content-Type', function (val) { return val && val[0] === 'application/x-www-form-urlencoded'; })
+        .get('/token')
         .reply(
           200,
           'me=http%3A%2F%2Fkodfabrik.se%2F&issued_by=https%3A%2F%2Ftokens.indieauth.com%2Ftoken&client_id=http%3A%2F%2F127.0.0.1%3A8080%2F&issued_at=1435611612&scope=post&nonce=501574078',
           { 'Content-Type': 'application/x-www-form-urlencoded' }
         );
 
-      agent
-        .post('/micropub/thepost.se')
-        .set('Authorization', 'Bearer ' + token)
-        .type('form')
-        .send({
-          h: 'entry',
-          content: 'hello world',
-        })
-        .expect(201, function (err) {
-          if (err) { return done(err); }
-          mock.done();
-          done();
-        });
+      doRequest(mock, done);
+    });
+
+    it('should return error on invalid token', function (done) {
+      var mock = mockTokenEndpoint(400, 'error=unauthorized&error_description=The+token+provided+was+malformed');
+      doRequest(mock, done, 403);
+    });
+
+    it('should return error on mismatching me', function (done) {
+      var mock = mockTokenEndpoint(200, 'me=http%3A%2F%2Fvoxpelli.com%2F&scope=post');
+      doRequest(mock, done, 403);
+    });
+
+    it('should return error on missing post scope', function (done) {
+      var mock = mockTokenEndpoint(200, 'me=http%3A%2F%2Fkodfabrik.se%2F&scope=misc');
+      doRequest(mock, done, 403);
+    });
+
+    it('should handle multiple scopes', function (done) {
+      var mock = mockTokenEndpoint(200, 'me=http%3A%2F%2Fkodfabrik.se%2F&scope=post,misc');
+      doRequest(mock, done);
     });
 
   });
+
+  describe('create', function () {
+
+    it('should require content', function (done) {
+      var mock = mockTokenEndpoint(200, 'me=http%3A%2F%2Fkodfabrik.se%2F&scope=post,misc');
+      doRequest(mock, done, 400, {
+        h: 'entry',
+      });
+    });
+
+  });
+
 });
