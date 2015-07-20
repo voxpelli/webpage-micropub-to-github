1e8329d590efe11c1014cf0183b855e634982c2c
Pelle Wessman <pelle@kodfabrik.se>
2015-07-20 12:48:46 +0200

    Support file uploading

--- test/handler.spec.js
+++ test/handler.spec.js
@@ -71,5 +71,66 @@ describe('Handler', function () {
         });
     });
 
+    it('should upload files prior to content', function () {
+      var token = 'abc123';
+      var user = 'username';
+      var repo = 'repo';
+      var repoPath = '/repos/' + user + '/' + repo + '/contents/';
+      var mediaFilename = 'foo.jpg';
+
+      var fileContent = new Buffer('abc123');
+
+      var encodedContent = new Buffer(
+        '---\n' +
+        'layout: micropubpost\n' +
+        'date: \'2015-06-30T14:20:00.000Z\'\n' +
+        'title: awesomeness is awesome\n' +
+        'slug: awesomeness-is-awesome\n' +
+        'mf-photo:\n' +
+        '  - \'http://example.com/foo/media/2015-06-awesomeness-is-awesome/' + mediaFilename + '\'\n' +
+        '---\n' +
+        'hello world\n'
+      );
+
+      var mock = nock('https://api.github.com/')
+        .matchHeader('authorization', function (val) { return val && val[0] === 'Bearer ' + token; })
+        // Upload of the media
+        .put(repoPath + 'media/2015-06-awesomeness-is-awesome/' + mediaFilename, {
+          //TODO: Change this commit message to at least be "new media" instead
+          message: 'new content',
+          content: fileContent.toString('base64'),
+        })
+        .reply(201, { content : { sha : 'abc123' } })
+        // Upload of the content
+        .put(repoPath + '_posts/2015-06-awesomeness-is-awesome.html', {
+          message: 'new content',
+          content: encodedContent.toString('base64'),
+        })
+        .reply(201, { content : { sha : 'abc123' } });
+
+      return handler(
+          {
+            token: token,
+            user: user,
+            repo: repo,
+          }, {
+            'type': ['h-entry'],
+            'properties': {
+              'content': ['hello world'],
+              'name': ['awesomeness is awesome'],
+            },
+            files: {
+              photo: [{filename: 'foo.jpg', buffer: fileContent}]
+            }
+          },
+          'http://example.com/foo/'
+        )
+        .then(function (url) {
+          mock.done();
+          url.should.equal('http://example.com/foo/2015/06/awesomeness-is-awesome/');
+        });
+    });
+
+
   });
 });
