ae45a771e28bd09d2640fa2f3c9def19974c2abd
Pelle Wessman <pelle@kodfabrik.se>
2015-07-22 12:29:07 +0200

    Only override existing content on matching URL

--- test/handler.spec.js
+++ test/handler.spec.js
@@ -131,6 +131,107 @@ describe('Handler', function () {
         });
     });
 
+    it('should override existing content if matching URL', function () {
+      var token = 'abc123';
+      var user = 'username';
+      var repo = 'repo';
+      var path = '/repos/' + user + '/' + repo + '/contents/_posts/2015-06-30-awesomeness-is-awesome.md';
+      var sha = 'abc123';
+
+      var encodedContent = new Buffer(
+        '---\n' +
+        'layout: micropubpost\n' +
+        'date: \'2015-06-30T14:20:00.000Z\'\n' +
+        'title: awesomeness is awesome\n' +
+        'slug: awesomeness-is-awesome\n' +
+        '---\n' +
+        'hello world\n'
+      );
+      var base64 = encodedContent.toString('base64');
+
+      var mock = nock('https://api.github.com/')
+        .put(path, {
+          message: 'uploading article',
+          content: base64,
+        })
+        .reply(422, {})
+
+        .get(path)
+        .reply(200, {sha: sha})
+
+        .put(path, {
+          message: 'uploading article',
+          content: base64,
+          sha: sha,
+        })
+        .reply(201, { content : { sha : 'xyz789' } });
+
+      return handler(
+          {
+            token: token,
+            user: user,
+            repo: repo,
+          }, {
+            'type': ['h-entry'],
+            'properties': {
+              'content': ['hello world'],
+              'name': ['awesomeness is awesome'],
+              'url': 'http://example.com/foo/2015/06/awesomeness-is-awesome/',
+            },
+          },
+          'http://example.com/foo/'
+        )
+        .then(function (url) {
+          mock.done();
+          url.should.equal('http://example.com/foo/2015/06/awesomeness-is-awesome/');
+        });
+    });
+
+    it('should not override existing content if no matching URL', function () {
+      var token = 'abc123';
+      var user = 'username';
+      var repo = 'repo';
+      var path = '/repos/' + user + '/' + repo + '/contents/_posts/2015-06-30-awesomeness-is-awesome.md';
+
+      var encodedContent = new Buffer(
+        '---\n' +
+        'layout: micropubpost\n' +
+        'date: \'2015-06-30T14:20:00.000Z\'\n' +
+        'title: awesomeness is awesome\n' +
+        'slug: awesomeness-is-awesome\n' +
+        '---\n' +
+        'hello world\n'
+      );
+      var base64 = encodedContent.toString('base64');
+
+      var mock = nock('https://api.github.com/')
+        .put(path, {
+          message: 'uploading article',
+          content: base64,
+        })
+        .reply(422, {});
+
+      return handler(
+          {
+            token: token,
+            user: user,
+            repo: repo,
+          }, {
+            'type': ['h-entry'],
+            'properties': {
+              'content': ['hello world'],
+              'name': ['awesomeness is awesome'],
+            },
+          },
+          'http://example.com/foo/'
+        )
+        .then(function (url) {
+          mock.done();
+          url.should.equal(false);
+        });
+    });
+
 
   });
+
 });
